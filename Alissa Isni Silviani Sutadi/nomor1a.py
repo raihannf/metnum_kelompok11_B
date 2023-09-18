3a7ae7ca19c496259440e02966de29a5bafa08d4
alissais <125477768+Alissaisni@users.noreply.github.com>
2023-09-19 00:12:08 +0700

    nomor1a.py

--- Alissa Isni Silviani Sutadi/nomor1a.py
+++ Alissa Isni Silviani Sutadi/nomor1a.py
@@ -1,20 +1,27 @@
+import numpy as np  # Memanggil library numpy
+
 def f(x):
-    #Soal nomor 1a
     return x**3 - 2*x + 1
 
-def bisection_iterasi(N):
-    x = 0 #nilai x dari awal
-    Iterasi = 0 #Menginisialisasi Iterasi
-    #looping mencari iterasi
-    while Iterasi < N: 
-        #mencari akar
-        x_New = x - f(x) / (3*x**2 - 2)
-        if abs(x_New - x) < 1e-6: 
-            break
-        x = x_New
-        Iterasi += 1
-    return x
+def my_bisection(a, b, eps):
+    if np.sign(f(a)) == np.sign(f(b)):
+        raise Exception('Tidak ada akar pada interval a dan b')
 
-N = 3 
-root = bisection_iterasi(N)
-print(f"Akar hampirannya adalah: {root}")
+    iterasi = 0
+    while True:  # Terus berjalan hingga kondisi berhenti terpenuhi
+        x = (a + b) / 2  # Titik tengah interval
+        if np.abs(f(x)) < eps:  # Kondisi berhenti jika toleransi terpenuhi
+            return x
+
+        # Memilih interval baru berdasarkan tanda f(x)
+        if np.sign(f(a)) == np.sign(f(x)):
+            a = x
+        else:
+            b = x
+
+        iterasi += 1
+
+# Mencari akar dalam interval [0, 1] dengan toleransi 1e-5
+akarHampiran = my_bisection(0, 1, 1e-5)
+
+print("Akar Hampiran dari f:", akarHampiran)
