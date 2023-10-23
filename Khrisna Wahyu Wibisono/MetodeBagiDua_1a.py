c5136b7fc914087454d15ff904c05877e339480e
Khrisna Wahyu Wibisono <145247951+KhrisnaWahyu@users.noreply.github.com>
2023-10-23 20:50:46 +0700

    MetodeBagiDua_1a.py

--- Khrisna Wahyu Wibisono/MetodeBagiDua_1a.py
+++ Khrisna Wahyu Wibisono/MetodeBagiDua_1a.py
@@ -2,25 +2,30 @@
 # NIM: 2204340
 # Buatlah sebuah fungsi penyelesaian yang tepat baik dengan menggunakan metode Bagi Dua ketika
 # a. f(x) = x^3 - 2x + 1
-# Buatlah modifikasi fungsi ketika kriteria program berhenti adalah sudah mencapai pada iterasi ke-n
 
-def f(x): #Menisialisasi fungsi yang digunakan
-    return x**3 - 2*x + 1 #fungsi f(x) = x^3 - 2x + 1
 
-def BagiDua_Iterasi_N(N):
-    x = 0  # Nilai awal dari x
-    Iterasi = 0  #Inisialisasi hitungan iterasi
+import numpy as np  #Library Numpy digunakan untuk membantu perhitungan
 
-    #Melakukan Perulangan untuk mencari Iterasi k-N
-    while Iterasi < N: 
-        x_new = x - f(x) / (3*x**2 - 2)  # Metode untuk mencari akar
-        if abs(x_new - x) < 1e-6:  # Kondisi dimana program akan berhenti jika perbedaan sangat kecil
-            break
-        x = x_new
-        Iterasi += 1
+def f(x):
+    return x**3-2*x+1  #Menisialisasi fungsi yang digunakan
 
-    return x
+def my_bisection(a, b, eps):
+  #Memeriksa tanda fungsi pada titik awal interval (a, b).
+  if np.sign(f(a)) == np.sign(f(b)):
+    raise Exception('Tidak ada akar pada interval a dan b')  #jika f(a) dan f(b) memiliki tanda yang sama, Raise Pesan
+  
+  #Proses melakukan pencarian akar
+  while np.abs(b-a) > eps:  #Dengan menggunakan perulangan while,melakukan iterasi selama selisih b dan a lebih besar dari ketelitian (epsilon)
+    c = (a + b)/2
+    if f(c) == 0: 
+      return c
+    elif f(a) * f(c) < 0:
+      return my_bisection(a, c, eps)
+    else:
+      return my_bisection(c, b, eps)
+  return (a + b)/2
+    
+#Mencari akar menggunakan fungsi my_bisection dengan memberikan interval (a, b) dan nilai epsilon
+akarHampiran = my_bisection(0, 1, 1e-6)
 
-N = 3  # Ganti dengan nilai n yang Anda inginkan
-root = BagiDua_Iterasi_N(N)
-print(f"Akar setelah iterasi ke {N} adalah: {root}")
+print("Akar Hampiran dari f:", akarHampiran)  #Menampilkan Output hasil pencarian akar
